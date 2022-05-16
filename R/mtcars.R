09cf404d0138bd474d54b8a8c58da040405972e6
Jinseob Kim <jinseob2kim@gmail.com>
2022-05-16 11:01:22 +0000

    d

---
+++ R/mtcars.R
@@ -0,0 +1,18 @@
+#' @title DATASET_TITLE
+#' @description DATASET_DESCRIPTION
+#' @format A data frame with 32 rows and 11 variables:
+#' \describe{
+#'   \item{\code{mpg}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{cyl}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{disp}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{hp}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{drat}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{wt}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{qsec}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{vs}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{am}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{gear}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{carb}}{double COLUMN_DESCRIPTION} 
+#'}
+#' @details DETAILS
+"mtcars"
